d724ccde8a66c4515ecc23cf13c4999ac35c516d
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-08-26 14:04:54 +0900

    fix : endpoint prefix 수정

--- src/main/java/com/lgcms/member/api/open/OpenAdminMemberController.java
+++ src/main/java/com/lgcms/member/api/open/OpenAdminMemberController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
-@RequestMapping("/api/admin/member")
+@RequestMapping("/admin/member")
 @RestController
 @RequiredArgsConstructor
 public class OpenAdminMemberController {
